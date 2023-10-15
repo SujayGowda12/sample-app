b0b5a7eb183c35902c5fd69ab9f4ed60e029c928
ambreenhussain <ambreen.hussain@bcu.ac.uk>
2023-10-15 11:47:33 +0100

    Changed port from 8080 to 5000.

--- web-app.sh
+++ web-app.sh
@@ -15,7 +15,7 @@ echo "COPY      ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY      ./templates /home/myapp/templates/" >> tempdir/Dockerfile 
 echo "COPY      sample_app.py /home/myapp/" >> tempdir/Dockerfile
 
-echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "EXPOSE 5000" >> tempdir/Dockerfile
 
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
@@ -23,6 +23,6 @@ cd tempdir
 
 docker build -t sampleapp .
 
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp    
+docker run -t -d -p 5000:5000 --name samplerunning sampleapp    
 
 docker ps -a
